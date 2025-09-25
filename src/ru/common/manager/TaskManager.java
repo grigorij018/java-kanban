46098c990233272be63e2e1a60982b3f18a7b7e9
grigorij018 <grigorij018@mail.ru>
2025-09-25 18:14:51 +0300

    fix: add empty lines between methods in TaskManager interface

--- src/ru/common/manager/TaskManager.java
+++ src/ru/common/manager/TaskManager.java
@@ -7,25 +7,41 @@ import java.util.List;
 
 public interface TaskManager {
     List<Task> getAllTasks();
+
     void deleteAllTasks();
+
     Task getTask(int id);
+
     Task createTask(Task task);
+
     void updateTask(Task task);
+
     void deleteTask(int id);
 
     List<Epic> getAllEpics();
+
     void deleteAllEpics();
+
     Epic getEpic(int id);
+
     Epic createEpic(Epic epic);
+
     void updateEpic(Epic epic);
+
     void deleteEpic(int id);
 
     List<SubTask> getAllSubtasks();
+
     void deleteAllSubtasks();
+
     SubTask getSubtask(int id);
+
     SubTask createSubtask(SubTask subtask);
+
     void updateSubtask(SubTask subtask);
+
     void deleteSubtask(int id);
+
     List<SubTask> getSubtasksByEpic(int epicId);
 
     List<Task> getHistory();
